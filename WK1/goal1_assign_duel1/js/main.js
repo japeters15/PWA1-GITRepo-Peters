fb3d00a615e45f784a2954b554810f993d32e9f2
Janay Peters <japeters15@gmail.com>
2015-08-08 00:20:17 -0400

    WK1/goal1_assign_duel1
    allowing the numbers to decrease as the players fight

--- WK1/goal1_assign_duel1/js/main.js
+++ WK1/goal1_assign_duel1/js/main.js
@@ -25,7 +25,23 @@
 
     function fight(){
         console.log('in the fight function')
-        winnerCheck()
+
+        alert(playerOneName+":"+playerOneHealth+"START"+playerTwoName+":"+playerTwoHealth);
+
+        for(var i=0;i<10; i++){
+
+            var minDamage1 = player1Damage * .5;
+            var minDamage2 = player2Damage * .5;
+            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+            //inflict damage
+            playerOneHealth-=f1;
+            playerTwoHealth-=f2;
+
+            console.log(playerOneHealth);
+            console.log(playerTwoHealth);
+        }
     };
 
     function winnerCheck(){
