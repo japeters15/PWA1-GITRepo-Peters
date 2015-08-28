6727ac0b338a3a45863f984de92039d53f7fa38a
Janay Peters <japeters15@gmail.com>
2015-08-28 07:55:08 -0400

    WK4/goal7_assign_obj_to_prototypes
    Created most of my pseudo code

--- WK4/goal7_assign_obj_to_prototypes/js/main.js
+++ WK4/goal7_assign_obj_to_prototypes/js/main.js
@@ -2,24 +2,38 @@
  * Created by the JavaScript Development Team
  * Class: PWA
  * Goal: Goal7
+ * Janay Peters
  */
 
-Create an array called names that contains at least 5 people names that will be randomly selected in the output display.
+//Create an array called "Names" will contains at 5 names:
+// Janay
+// Danielle
+// Chare
+// Nina
+// Jude
+// Will be randomly selected in the output display.
 
-    Create three instances of the Person object using a FOR loop and place each reference of the object in an array called people.
 
-    When instantiating the Person object, make sure a randomly chosen name (using the Math.random() method) from the names array is sent to the constructor of the person along with what row number in the HTML the information will be displayed in.
-Ex: var person = new Person(names[someName],someRow);
 
-Create a function called "populateHTML" which outputs the person's name and person's job, on the web page.
+//Create a "FOR loop" and place each reference of the object in an array called "People".
 
-    The code must be built to NOT allow duplicate names to appear.
+//randomly chose a name (using the Math.random() method)
+// the names array is sent to the constructor of the person along with what row number in the HTML the information will be displayed in.
+        Ex: var person = new Person(names[someName],someRow);
 
-    Set up an Interval that calls a runUpdate() function 30 times a second.
+
+
+//Create a function called "populateHTML" (outputs the person's name and person's job, on the web page.)
+//NOT allow duplicate names to appear.!!!
+
+//Set up an Interval that calls a runUpdate() function 30 times a second.
     Example: "setInterval(runUpdate, 1000 / 30);"
+//Passing two arguments
 
-Once all the Person instances are in the people array, the program will loop through each person and run the prototype update() function.
-The loop should execute in the runUpdate() function that is called from the setInterval (in the example directly above). For example:
+
+//allow program to loop through each person and run the prototype update() function.
+//should execute in the runUpdate() function that is called from the setInterval
+// For example:
     function runUpdate(){
         people.forEach(function(element){
 
