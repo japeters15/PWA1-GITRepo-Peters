3776cdf612ed3a0a6561ea77133877c941168a5b
Janay Peters <japeters15@gmail.com>
2015-08-28 12:00:12 -0400

    WK4/goal7_assign_obj_to_prototypes
    Created flow chart for person.js

--- WK4/goal7_assign_obj_to_prototypes/js/person.js
+++ WK4/goal7_assign_obj_to_prototypes/js/person.js
@@ -15,7 +15,7 @@
 //3.The Person constructor should also have four properties:
     //name: The name of the person. (This property is set in this constructor for the Person.)
     //action: for the Person. This property states what the person is actively doing and it is one of the values in the actions array.
-    //Select one item from the Person.action array for this property (Math.random() method).
+//Select one item from the Person.action array for this property (Math.random() method).
     //job: for the Person and is one of the values in the jobs array. ( Math.random() method).
     //row: like the name property, this is set in this constructor.
 
