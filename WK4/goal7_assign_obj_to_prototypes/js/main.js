a8cbea94db462562611e1e3c4c22c6986f0aa9f0
Janay Peters <japeters15@gmail.com>
2015-08-28 11:07:46 -0400

    WK4/goal7_assign_obj_to_prototypes
    Edited pseudo code

--- WK4/goal7_assign_obj_to_prototypes/js/main.js
+++ WK4/goal7_assign_obj_to_prototypes/js/main.js
@@ -5,7 +5,7 @@
  * Janay Peters
  */
 
-//Create an array called "Names" will contains at 5 names:
+//1.Create an array called "Names" will contains at 5 names:
 // Janay
 // Danielle
 // Chare
@@ -16,27 +16,29 @@
 Ex.  var names = [
               "Janay",
               "Danielle",
-              "Nina"
+              "Nina",
+              "Chare",
+              "Jude"
         ];
 
 
-//Create a "FOR loop" and place each reference of the object in an array called "People".
+//2.Create a "FOR loop" and place each reference of the object in an array called "People".
 
-//randomly chose a name (using the Math.random() method)
+//3.randomly chose a name (using the Math.random() method)
 // the names array is sent to the constructor of the person along with what row number in the HTML the information will be displayed in.
-        Ex: var person = new Person(names[someName],someRow);
+        Ex: var person = new Person(names[2],someRow);
 
 
-//Create a function called "populateHTML" (outputs the person's name and person's job, on the web page.)
+//4.Create a function called "populateHTML" (outputs the person's name and person's job, on the web page.)
 //NOT allow duplicate names to appear.!!!
 
 
-//Set up an Interval that calls a runUpdate() function 30 times a second.
+//5.Set up an Interval that calls a runUpdate() function 30 times a second.
     Example: "setInterval(runUpdate, 1000 / 30);"
 //Passing two arguments
 
 
-//allow program to loop through each person and run the prototype update() function.
+//6.allow program to loop through each person and run the prototype update() function.
 //should execute in the runUpdate() function that is called from the setInterval
 // For example:
     function runUpdate(){
--- WK4/goal7_assign_obj_to_prototypes/js/person.js
+++ WK4/goal7_assign_obj_to_prototypes/js/person.js
@@ -4,21 +4,21 @@
  * Goal: Goal7
  */
 
-//Add the Person object to the global window object
+//1.Add the Person object to the global window object
        Example: "window.Person=Person;"
 
 //Two variables, "jobs" and "actions"
-//The jobs variable is an array of 4 or more jobs. The actions variable is also an array of some actions a person could do.
+//2.The jobs variable is an array of 4 or more jobs. The actions variable is also an array of some actions a person could do.
     ex: Person.jobs = ["teacher","farmer","student","pilot"];
     ex: Person.actions = ["sleeping","eating","working"];
 
-//The Person constructor should also have four properties:
+//3.The Person constructor should also have four properties:
     //name: The name of the person. (This property is set in this constructor for the Person.)
     //action: for the Person. This property states what the person is actively doing and it is one of the values in the actions array.
     //Select one item from the Person.action array for this property (Math.random() method).
     //job: for the Person and is one of the values in the jobs array. ( Math.random() method).
     //row: like the name property, this is set in this constructor.
 
-//Display the initial action of the person in 3rd column.
-//The Person object must have an update() function added to its prototype. (called from the main.js file)
+//4.Display the initial action of the person in 3rd column.
+//5.The Person object must have an update() function added to its prototype. (called from the main.js file)
 
